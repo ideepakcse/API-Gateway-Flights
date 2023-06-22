ef068616b28fc4c2f55a7e7bd5a4db4ff4c5e3bb
ideepakcse <ideepakcse@gmail.com>
2023-06-22 12:57:29 +0530

    implemented rate limiter and reverse proxy

--- src/config/server-config.js
+++ src/config/server-config.js
@@ -6,5 +6,7 @@ module.exports = {
     PORT: process.env.PORT,
     SALT: bcrypt.genSaltSync(17),
     JWT_KEY: process.env.JWT_KEY,
-    JWT_EXPIRY: process.env.JWT_EXPIRY
+    JWT_EXPIRY: process.env.JWT_EXPIRY,
+    FLIGHT_SERVICE: process.env.FLIGHT_SERVICE,
+    BOOKING_SERVICE: process.env.BOOKING_SERVICE
 }
--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 const express=require('express');
+const rateLimit = require('express-rate-limit');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const {ServerConfig}=require('./config');
 
@@ -7,13 +9,31 @@ const apiRoutes=require('./routes');
 
 const app = express();
 
+
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 2 minutes
+	max: 100, // Limit each IP to 2 requests per `window` (here, per 15 minutes)
+});
+
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+app.use(limiter);
 
 app.use('/api',apiRoutes);
+
+//app.use('/api', createProxyMiddleware({ target: 'http://www.example.org', changeOrigin: true }));
+app.use('/flightsService', createProxyMiddleware({ target: ServerConfig.FLIGHT_SERVICE, changeOrigin: true, }));
+app.use('/bookingService', createProxyMiddleware({ target: ServerConfig.BOOKING_SERVICE, changeOrigin: true, }));
 
 app.listen(ServerConfig.PORT, () => {
     console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
 });
 
-
+/**
+ * user
+ *  |
+ *  v
+ * from : localhost:3001 (API Gateway)  to---> localhost:4000/api/v1/bookings
+ * 
+ *                                      to---> localhost:3000/api/v1/flights
+ */
